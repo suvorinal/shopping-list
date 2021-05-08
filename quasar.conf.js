547efd384d997eab9bc5003f76324b73f7c2df7f
artem1syvorin <kanalartemona@gmail.com>
2021-05-08 15:26:37 +0500

    add PWA support

--- quasar.conf.js
+++ quasar.conf.js
@@ -146,7 +146,7 @@ module.exports = configure(function (ctx) {
                 display: 'standalone',
                 orientation: 'portrait',
                 background_color: '#ffffff',
-                theme_color: '#027be3',
+                theme_color: '#1e88e5',
                 icons: [
                     {
                         src: 'icons/icon-128x128.png',
